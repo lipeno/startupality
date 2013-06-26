947e9859a90679ce1afce2d6723ad16fc6dab8ce
Andrej <andrejdragisic@gmail.com>
2013-06-26 20:41:43 +0200

    removed pdfkit

--- app/models/expense.rb
+++
@@ -1,4 +0,0 @@
-class Expense < ActiveRecord::Base
-  attr_accessible :april, :august, :december, :february, :january, :july, :june, :march, :may, :november, :october, :project_id, :rowName, :rowNumber, :september, :year
-  belongs_to :project
-end
