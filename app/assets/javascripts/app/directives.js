11939db5705c4d48c9042b65f7173faab6b01a05
Andrej <andrejdragisic@gmail.com>
2013-10-15 19:58:28 +0200

    Updated drag n drop with new directive

--- app/assets/javascripts/app/directives.js
+++ app/assets/javascripts/app/directives.js
@@ -147,77 +147,55 @@ app.directive('calendarYear', function () {
 
 });
 
-// directive for drag and drop
-app.directive('dndBetweenList', function($parse) {
- 
-    return function(scope, element, attrs) {
- 
-        // contains the args for this component
-        var args = attrs.dndBetweenList.split(',');
-        // contains the args for the target
-        var targetArgs = $('#'+args[1]).attr('dnd-between-list').split(',');
-		
-		
-		alert(args[0]);
-		alert(targetArgs[0]);
-		
- 
-        // variables used for dnd
-        var toUpdate;
-        var target;
-        var startIndex = -1;
-        var toTarget = true;
- 
-        // watch the model, so we always know what element
-        // is at a specific position
-        scope.$watch(args[0], function(value) {
-            toUpdate = value;
-        },true);
- 
-        // also watch for changes in the target list
-        scope.$watch(targetArgs[0], function(value) {
-            target = value;
-        },true);
-		
- 
-        // use jquery to make the element sortable (dnd). This is called
-        // when the element is rendered
-        $(element[0]).sortable({
-            items:'li',
-            start:function (event, ui) {
-                // on start we define where the item is dragged from
-                startIndex = ($(ui.item).index());
-                toTarget = false;
-            },
-            stop:function (event, ui) {
-                var newParent = ui.item[0].parentNode.id;
-				
 
- 
-                // on stop we determine the new index of the
-                // item and store it there
-                var newIndex = ($(ui.item).index());
-                var toMove = toUpdate[startIndex];
- 
-                // we need to remove him from the configured model
-                toUpdate.splice(startIndex,1);
- 
-	            if (newParent == args[1]) {
-                    // and add it to the linked list
-                    target.splice(newIndex,0,toMove);
-                }  else {
-                    toUpdate.splice(newIndex,0,toMove);
+app.directive('sortable', function(){
+    return {
+        restrict: 'A',
+        require: '?ngModel',
+        link: function(scope, element, attrs, ngModel){
+            if (ngModel){
+                ngModel.$render = function(){
+                    element.sortable( 'refresh' );
+                };
+            }
+
+            element.sortable({
+                connectWith: attrs.sortableSelector,
+                cancel: 'a',
+                revert: true,
+                remove: function(e, ui){
+                    if (ngModel.$modelValue.length === 1) {
+                        ui.item.sortable.moved = ngModel.$modelValue.splice(0, 1)[0];
+                    } else {
+                        ui.item.sortable.moved =  ngModel.$modelValue.splice(ui.item.sortable.index, 1)[0];
+                    }
+                },
+                receive: function(e, ui){
+                    ui.item.sortable.relocate = true;
+                    ngModel.$modelValue.splice(ui.item.index(), 0, ui.item.sortable.moved);
+                },
+                update: function(e, ui){
+                    ui.item.sortable.resort = ngModel;
+                },
+                start: function(e, ui){
+                    ui.item.sortable = { index: ui.item.index(), resort: ngModel };
+                },
+                stop: function(e, ui){
+                    if (ui.item.sortable.resort && !ui.item.sortable.relocate){
+                        var end, start;
+                        start = ui.item.sortable.index;
+                        end = ui.item.index();
+
+                        ui.item.sortable.resort.$modelValue.splice(end, 0, ui.item.sortable.resort.$modelValue.splice(start, 1)[0]);
+                    }
+                    if (ui.item.sortable.resort || ui.item.sortable.relocate) {
+                        scope.$apply();
+                    }
+                    setTimeout()
                 }
- 
-                // we move items in the array, if we want
-                // to trigger an update in angular use $apply()
-                // since we're outside angulars lifecycle
-                scope.$apply(targetArgs[0]);
-                scope.$apply(args[0]);
-            },
-            connectWith:'.aaaaa'
-        })
-    }
+            });
+        }
+    };
 });
 
 
--- app/assets/javascripts/controllers/goals.js
+++ app/assets/javascripts/controllers/goals.js
@@ -91,82 +91,45 @@ app.controller('GoalsController', function ($scope, CurrentProject, Card){
 		card.$update({projectId: $scope.currentProject.id});
 		
     };
-	
-	
-	
-	
-	
-	$scope.model = [
-	        {
-	            "id": 1,
-	            "value": "Who the fuck is Arthur Digby Sellers?"
-	        },
-	        {
-	            "id": 2,
-	            "value": "I've seen a lot of spinals, Dude, and this guy is a fake. "
-	        },
-	        {
-	            "id": 3,
-	            "value": "But that is up to little Larry here. Isn't it, Larry?"
-	        },
-	        {
-	            "id": 4,
-	            "value": " I did not watch my buddies die face down in the mud so that this fucking strumpet."
-	        }
-	    ];
- 
-	    $scope.source = [
-	        {
-	            "id": 5,
-	            "value": "What do you mean \"brought it bowling\"? I didn't rent it shoes."
-	        },
-	        {
-	            "id": 6,
-	            "value": "Keep your ugly fucking goldbricking ass out of my beach community! "
-	        },
-	        {
-	            "id": 7,
-	            "value": "What the fuck are you talking about? I converted when I married Cynthia!"
-	        },
-	        {
-	            "id": 8,
-	            "value": "Ja, it seems you forgot our little deal, Lebowski."
-	        }
-	    ];
-		
-	    $scope.olal = [
-	        {
-	            "id": 9,
-	            "value": "What do you mean \"brought it bowling\"? I didn't rent it shoes."
-	        }
-		];
- 
-	    // watch, use 'true' to also receive updates when values
-	    // change, instead of just the reference
-	    $scope.$watch("model", function(value) {
-	        console.log("Model: " + value.map(function(e){return e.id}).join(','));
-	    },true);
- 
-	    // watch, use 'true' to also receive updates when values
-	    // change, instead of just the reference
-	    $scope.$watch("source", function(value) {
-	        console.log("Source: " + value.map(function(e){return e.id}).join(','));
-	    },true);
-		
-	    $scope.$watch("olal", function(value) {
-	        console.log("Source: " + value.map(function(e){return e.id}).join(','));
-	    },true);
-		
-		
-	    $scope.sourceEmpty = function() {
-	           return $scope.source.length == 0;
-	       }
- 
-	       $scope.modelEmpty = function() {
-	           return $scope.model.length == 0;
-	       }
-	
-	
+
+
+    $scope.columnsWithItems = {'Column 1':{'items':['Item 1', 'Item 2']}, 'Column 2':{ 'items': ['Item 3', 'Item 4'] }, 'Column 3':{ 'items': ['Item 5', 'Item 6']}};
+
+    $scope.doStuff = function(item){
+        console.log("clicked", item);
+    };
+
+    $scope.$watch('board1', function(newvalue) {
+        var newElement = _.find(newvalue, function(item){ return item.board !== "board1"; });
+        if (newElement) {
+            newElement.board = "board1";
+            newElement.$update({projectId: $scope.currentProject.id});
+        }
+    }, true);
+
+    $scope.$watch('board2', function(newvalue) {
+        var newElement = _.find(newvalue, function(item){ return item.board !== "board2"; });
+        if (newElement) {
+            newElement.board = "board2";
+            newElement.$update({projectId: $scope.currentProject.id});
+        }
+    }, true);
+
+    $scope.$watch('board3', function(newvalue) {
+        var newElement = _.find(newvalue, function(item){ return item.board !== "board3"; });
+        if (newElement) {
+            newElement.board = "board3";
+            newElement.$update({projectId: $scope.currentProject.id});
+        }
+    }, true);
+
+    $scope.$watch('board4', function(newvalue) {
+        var newElement = _.find(newvalue, function(item){ return item.board !== "board4"; });
+        if (newElement) {
+            newElement.board = "board4";
+            newElement.$update({projectId: $scope.currentProject.id});
+        }
+    }, true);
 	
 	
 	
